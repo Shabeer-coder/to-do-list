ac0896083c021feb53911a0182f552aaeb65e147
Shabeer-coder <shabeermuhammed088@gmail.com>
2025-03-25 10:42:08 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainPageComponent
+  ],
+  imports: [
+    BrowserModule, 
+    BrowserAnimationsModule,  
+    FormsModule ,
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
